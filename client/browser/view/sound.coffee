403783075e8c473a80bfed9f8c1db3842516ec41
Valuks <termilv@gmail.com>
2018-11-23 00:09:11 +0200

    sound stop

--- client/browser/view/sound.coffee
+++ client/browser/view/sound.coffee
@@ -1,16 +1,27 @@
 @o.Sound = class Sound
   constructor: (@options)->
+    @_media_last = null
     @__muted = false
+    fn = =>
+      @__enable = true
+      document.body.removeEventListener('click', fn)
+    document.body.addEventListener('click', fn)
 
   play: (sound)->
+    if !@__enable
+      return
     if @__muted
       return
     try
-      a = new Audio("#{@options.path}#{sound}.wav")
-      a.volume = 0.3
-      a.play()
+      @_media_last = new Audio("#{@options.path}#{sound}.wav")
+      @_media_last.volume = 0.3
+      @_media_last.play()
     catch
+
+  stop: ->
+    if @_media_last
+      @_media_last.volume = 0
 
   is_mute: -> @__muted
 
-  mute: (@__muted)->
+  mute: (@__muted)-> @stop()
--- client/browser/view/sound.js
+++ client/browser/view/sound.js
@@ -4,21 +4,36 @@
 
   this.o.Sound = Sound = class Sound {
     constructor(options) {
+      var fn;
       this.options = options;
+      this._media_last = null;
       this.__muted = false;
+      fn = () => {
+        this.__enable = true;
+        return document.body.removeEventListener('click', fn);
+      };
+      document.body.addEventListener('click', fn);
     }
 
     play(sound) {
-      var a;
+      if (!this.__enable) {
+        return;
+      }
       if (this.__muted) {
         return;
       }
       try {
-        a = new Audio(`${this.options.path}${sound}.wav`);
-        a.volume = 0.3;
-        return a.play();
+        this._media_last = new Audio(`${this.options.path}${sound}.wav`);
+        this._media_last.volume = 0.3;
+        return this._media_last.play();
       } catch (error) {
 
+      }
+    }
+
+    stop() {
+      if (this._media_last) {
+        return this._media_last.volume = 0;
       }
     }
 
@@ -28,6 +43,7 @@
 
     mute(__muted) {
       this.__muted = __muted;
+      return this.stop();
     }
 
   };
